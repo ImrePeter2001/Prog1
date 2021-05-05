073bd60d6a45648ec797922c7701483624ac1285
ImrePeter2001 <78748876+ImrePeter2001@users.noreply.github.com>
2021-05-05 15:59:41 +0200

    Update main.cpp

--- 19.drill/main.cpp
+++ 19.drill/main.cpp
@@ -39,7 +39,7 @@ ostream& operator<<(ostream& os, vector<T>& v)
     os<<"{";
     for(int i=0; i<v.size(); ++i)
     {
-        os<<v[i]<<(i<v.size()-1?", ":" ");
+        os<<v[i]<<(i<v.size()-1?", ");
     }
     os<<"}\n";
 
